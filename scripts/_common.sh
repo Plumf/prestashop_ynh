bf44e1d1b075285e86845a8575543e7276949c4f
Plumf <plumf@open-plug.eu>
2020-12-02 15:51:17 +0100

    update dependance

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,11 +4,12 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.3"
+YNH_DEFAULT_PHP_VERSION="7.3"
 
 # dependencies used by the app
-pkg_dependencies="php$YNH_DEFAULT_PHP_VERSION-zip"
+pkg_dependencies="php$YNH_DEFAULT_PHP_VERSION-curl php$YNH_DEFAULT_PHP_VERSION-xml php$YNH_DEFAULT_PHP_VERSION-mbstring php$YNH_DEFAULT_PHP_VERSION-zip"
 
+## En attente : php$YNH_DEFAULT_PHP_VERSION--with-gd php$YNH_DEFAULT_PHP_VERSION--enable-intl
 #=================================================
 # PERSONAL HELPERS
 #=================================================
